1ebf77f1d1a1be5cb88f7e13599424581688188f
manojshimpi <nodemy87@gmail.com>
2021-04-07 02:17:52 +0530

    add passport

--- src/router.js
+++ src/router.js
@@ -1,13 +1,50 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 const router = require('express').Router();
 require('./db/conn');
+
 const User = require('./models/usermessage');
+
 const ErrorHandler = require('./errors/ErrorHandler');
 
+// user autication code
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+const methodOverride = require('method-override');
+const initializePassport = require('./passport-config');
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
+
+//End autication code
+
+const users = [];
+
+// autication code
+
+router.use(flash())
+router.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+router.use(passport.initialize())
+router.use(passport.session())
+router.use(methodOverride('_method'))
+
+//End autication code
 
 // app.get() callback function
-router.get('/', (req, res) => {
+/*router.get('/', (req, res) => {
     res.render('index');
-})
+})*/
+
 
 router.get('/about', (req, res) => {
     res.render('about');
@@ -26,6 +63,51 @@ router.get('/team', (req, res) => {
 })
 router.get('/contact', (req, res) => {
     res.render('contact');
+})
+
+router.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('user/login');
+})
+
+router.get('/', (req, res) => {
+    //res.render('index');
+    console.log("sas");
+    res.render('home')
+})
+
+router.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+router.get('/register', checkNotAuthenticated, (req, res) => {
+
+    res.render('user/register');
+})
+router.post('/register', checkNotAuthenticated, async(req, res) => {
+    const { name, email, password } = req.body;
+    //console.log(username);
+    try {
+
+        const hashedPssword = await bcrypt.hash(password, 10);
+        users.push({
+            id: Date.now().toString,
+            name: name,
+            email: email,
+            password: hashedPssword
+        })
+        res.redirect('/login');
+    } catch {
+        res.redirect('/register');
+    }
+    console.log(users);
+
+})
+
+router.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
 })
 
 router.post('/contact', async(req, res, next) => {
@@ -55,5 +137,19 @@ router.post('/contact', async(req, res, next) => {
 
 });
 
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+
+    res.render('home')
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.render('home')
+    }
+    next()
+}
 
 module.exports = router;
